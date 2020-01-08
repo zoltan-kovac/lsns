339af7b9b7165650f492f4c731f74a6613df7a64
Zoltan Kovac <zoltan@macbook.local>
2020-01-08 15:49:58 +0100

    extend dataset

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,10 +1,12 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
-import posranci from '../poslanci.json'
+import poslanci from '../assets/data/poslanci.json'
+import kandidati from '../assets/data/kandidati.json'
 
 const PeopleList = () => (
   <div>
-    {posranci.map(({
+
+    <h2>Poslanci</h2>
+    {/* {poslanci.map(({
       addressCity,
       addressRegion,
       name,
@@ -17,7 +19,40 @@ const PeopleList = () => (
           <div><a href={photoUrl}>{name} ({addressRegion}, {addressCity})</a></div>
         )
     })}
-    {console.log(posranci)}
+
+    <h2>Kandidati</h2> */}
+    
+    {kandidati.data.map(({
+      name,
+      officialNumber,
+    }) => {
+      return (
+        <div className="fl w-25 pa2">
+          <div class="ba b--dotted db pa3 mb3">
+            {`${officialNumber}. ${name}`}
+
+            {poslanci.map(({
+              lastName,
+              addressCity,
+              addressRegion,
+              photoUrl,
+              partyDepartmentKey,
+            }) => {
+              const normalizedLastName = lastName.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
+              console.log(name.indexOf(normalizedLastName) !== -1)
+              if (name.indexOf(normalizedLastName) !== -1) {
+                return (
+                  // <a href={photoUrl}>()</a>
+                  <p>{addressRegion}, {addressCity}</p>
+                )
+              }
+            })}
+          </div>
+        </div>
+      )
+    })}
+
   </div>
 )
 
